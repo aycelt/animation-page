cda095b498650223c1a1ca7e798ccd2adbcac022
opeyemi Ayinla <yemiopeayinla@gmail.com>
2023-10-18 18:29:02 +0100

    first commit of all my files to the repository

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <main>
+
+        <img src="https://picsum.photos/800/300" alt="Placeholder">
+
+        <h1>About</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Nemo vitae ex sint doloribus porro voluptatem 
+            minima enim, illum quisquam temporibus.
+        </p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aliquam tempore soluta,
+             sapiente dolor facere, quidem doloribus neque veritatis temporibus ratione! Architecto
+             nemo eius inventore accusamus, quasi quaerat eum? Vel suscipit doloremque autem voluptate sequi.</p>
+    </main>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <main>
+        <h1>Home</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, nihil.
+        </p>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+             Eaque consectetur inventore ea, voluptatum adipisci impedit? Molestiae vitae magni iusto dolores itaque quaerat
+             inventore, natus facilis, hic veritatis harum officiis est!</p>
+    </main>
+</template>
